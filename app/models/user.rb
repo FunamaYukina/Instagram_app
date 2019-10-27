83f0b3c6a0fc1e9bede3cb567b70ecddd8587413
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-10-27 13:30:59 +0900

    [add] user_nameのuniqueness:true追加&Rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_secure_password
 
   before_save { self.email = email.downcase }
-  validates :user_name, presence: true
+  validates :user_name, presence: true, uniqueness: true
   validates :full_name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
