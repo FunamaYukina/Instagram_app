df9b23facc3810ea1c30a7b316ce8d0a7aba0bff
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-10-28 14:46:03 +0900

    [fix] Rubocopによる修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@
 
 class HomeController < ApplicationController
   def top
-    @users = User.eager_load([:posts,posts: :images])
+    @users = User.eager_load([:posts, posts: :images])
     @post = current_user.posts.build if logged_in?
   end
 end
