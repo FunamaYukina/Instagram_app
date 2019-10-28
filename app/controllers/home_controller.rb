64f1aac2a2ebdf3bdaebffa6d8b8144ed30de868
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-10-28 14:45:25 +0900

    [add] テーブル結合とログインユーザー名の表示まで

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@
 
 class HomeController < ApplicationController
   def top
-    @users = User.all
+    @users = User.eager_load([:posts,posts: :images])
     @post = current_user.posts.build if logged_in?
   end
 end
