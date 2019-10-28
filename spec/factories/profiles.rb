95f016e6822808337abcd37d7a40c3960bc3dd05
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-10-28 16:27:27 +0900

    [fix] Rubocopによる修正

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :profile do
-    image_file "MyString"
-    gender false
-    introduction "MyText"
+    image_file { "MyString" }
+    gender { false }
+    introduction { "MyText" }
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 require "support/utilities"
 
 RSpec.describe Post, type: :model do
-  describe "#create" do
+  describe "validation" do
     context "新規投稿に成功する場合" do
       it "メッセージがある場合投稿に成功すること" do
         post = FactoryBot.build(:post)
