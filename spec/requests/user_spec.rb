bb2d38e8414dc8985d969017451fc6fa4e4cc11d
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-11-02 15:24:01 +0900

    [fix] インデントの修正とRubocopによる修正

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe User, type: :request do
     context "新規登録ユーザー登録に成功する場合" do
       it "新規ユーザーが登録されること" do
         user_param = FactoryBot.attributes_for(:another_user)
-        post signup_path, params: { user: user_param }
+        post signup_path, params: {user: user_param}
         expect(response.status).to eq(302)
         expect(User.last.email).to eq user_param[:email]
         expect(response).to redirect_to(root_path)
@@ -44,7 +44,7 @@ RSpec.describe User, type: :request do
 
       it "メールアドレスは小文字で登録されること" do
         user_param = FactoryBot.attributes_for(:another_user)
-        post signup_path, params: { user: user_param }
+        post signup_path, params: {user: user_param}
         expect(response.status).to eq(302)
         expect(User.last.email).to eq "another_example@test.com"
         expect(response).to redirect_to(root_path)
@@ -55,7 +55,7 @@ RSpec.describe User, type: :request do
       it "メールアドレスがない場合、ユーザー登録に失敗すること" do
         expect do
           user_param = FactoryBot.attributes_for(:another_user, email: "")
-          post signup_path, params: { user: user_param }
+          post signup_path, params: {user: user_param}
         end.not_to change(User, :count)
       end
     end
@@ -69,7 +69,7 @@ RSpec.describe User, type: :request do
         log_in
         expect do
           user_param = FactoryBot.attributes_for(:another_user)
-          post signup_path, params: { user: user_param }
+          post signup_path, params: {user: user_param}
         end.not_to change(User, :count)
         expect(response).to redirect_to root_path
       end
@@ -106,30 +106,34 @@ RSpec.describe User, type: :request do
         signup
         log_in
       end
+
       it 'ユーザーネームを変更した場合、正しく更新されること' do
-        user_param = FactoryBot.attributes_for(:another_user,user_name:"test_user_name2")
-        patch profile_path(id: 1),params: {user: user_param}
+        user_param = FactoryBot.attributes_for(:another_user, user_name: "test_user_name2")
+        patch profile_path(id: 1), params: {user: user_param}
         expect(response.status).to eq(302)
         expect(User.last.user_name).to eq "test_user_name2"
         expect(response).to redirect_to(profile_path)
       end
+
       it 'フルネームを変更した場合、正しく更新されること' do
-        user_param = FactoryBot.attributes_for(:another_user,full_name:"test_full_name2")
-        patch profile_path(id: 1),params: {user: user_param}
+        user_param = FactoryBot.attributes_for(:another_user, full_name: "test_full_name2")
+        patch profile_path(id: 1), params: {user: user_param}
         expect(response.status).to eq(302)
         expect(User.last.full_name).to eq "test_full_name2"
         expect(response).to redirect_to(profile_path)
       end
+
       it 'メールアドレスを変更した場合、正しく更新されること' do
-        user_param = FactoryBot.attributes_for(:another_user,email:"example2@test.com")
-        patch profile_path(id: 1),params: {user: user_param}
+        user_param = FactoryBot.attributes_for(:another_user, email: "example2@test.com")
+        patch profile_path(id: 1), params: {user: user_param}
         expect(response.status).to eq(302)
         expect(User.last.email).to eq "example2@test.com"
         expect(response).to redirect_to(profile_path)
       end
+
       it 'パスワードを変更した場合、正しく更新されること' do
-        user_param = FactoryBot.attributes_for(:another_user,password:"test_password2",password_confirmation:"test_password2")
-        patch profile_path(id: 1),params: {user: user_param}
+        user_param = FactoryBot.attributes_for(:another_user, password: "test_password2", password_confirmation: "test_password2")
+        patch profile_path(id: 1), params: {user: user_param}
         expect(response.status).to eq(302)
         expect(User.last.password_digest).not_to eq nil
         expect(response).to redirect_to(profile_path)
@@ -140,27 +144,31 @@ RSpec.describe User, type: :request do
         signup
         log_in
       end
+
       it 'ユーザーネームがない場合、更新されないこと' do
-        user_param = FactoryBot.attributes_for(:another_user,user_name:"")
-        patch profile_path(id: 1),params: {user: user_param}
+        user_param = FactoryBot.attributes_for(:another_user, user_name: "")
+        patch profile_path(id: 1), params: {user: user_param}
         expect(response.status).to eq(200)
         expect(response.body).to include "ユーザーネームを入力してください"
       end
+
       it 'フルネームがない場合、更新されないこと' do
-        user_param = FactoryBot.attributes_for(:another_user,full_name:"")
-        patch profile_path(id: 1),params: {user: user_param}
+        user_param = FactoryBot.attributes_for(:another_user, full_name: "")
+        patch profile_path(id: 1), params: {user: user_param}
         expect(response.status).to eq(200)
         expect(response.body).to include "フルネームを入力してください"
       end
+
       it 'メールアドレスがない場合、更新されないこと' do
-        user_param = FactoryBot.attributes_for(:another_user,email:"")
-        patch profile_path(id: 1),params: {user: user_param}
+        user_param = FactoryBot.attributes_for(:another_user, email: "")
+        patch profile_path(id: 1), params: {user: user_param}
         expect(response.status).to eq(200)
         expect(response.body).to include "メールアドレスを入力してください"
       end
+
       it 'パスワードがない場合、更新されないこと' do
-        user_param = FactoryBot.attributes_for(:another_user,password:"")
-        patch profile_path(id: 1),params: {user: user_param}
+        user_param = FactoryBot.attributes_for(:another_user, password: "")
+        patch profile_path(id: 1), params: {user: user_param}
         expect(response.status).to eq(200)
         expect(User.last.password_digest).not_to eq nil
         expect(response.body).to include "パスワードは6文字以上で入力してください"
