8fa45529f39215b1adf45811ffcd448bc75970c8
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-11-10 17:21:08 +0900

    [fix] password_specにてuser_paramsをpassword_paramsへ変更

--- spec/requests/password_spec.rb
+++ spec/requests/password_spec.rb
@@ -13,11 +13,11 @@ RSpec.describe "Password", type: :request do
 
     context "(ログイン済みの場合で)パスワードの更新に成功する場合" do
       it "パスワードの更新に成功すること" do
-        user_params = FactoryBot.attributes_for(:user,
+        password_params = FactoryBot.attributes_for(:user,
                                                 current_password: "test_password",
                                                 password: "test_update_pass",
                                                 password_confirmation: "test_update_pass")
-        patch password_path, params: { user: user_params }
+        patch password_path, params: { user: password_params }
         expect(response.status).to eq(302)
         expect(response).to redirect_to(password_path)
       end
@@ -25,21 +25,21 @@ RSpec.describe "Password", type: :request do
 
     context "(ログイン済みの場合で)パスワードの更新に失敗する場合" do
       it "現在のパスワードが違う場合、更新されないこと" do
-        user_params = FactoryBot.attributes_for(:user,
+        password_params = FactoryBot.attributes_for(:user,
                                                 current_password: "test_wrong_password",
                                                 password: "test_update_pass",
                                                 password_confirmation: "test_update_pass")
-        patch password_path, params: { user: user_params }
+        patch password_path, params: { user: password_params }
         expect(response.status).to eq(200)
         expect(response.body).to include "現在のパスワードが違います"
       end
 
       it "新しいパスワードと再入力パスワードが異なる場合、更新されないこと" do
-        user_params = FactoryBot.attributes_for(:user,
+        password_params = FactoryBot.attributes_for(:user,
                                                 current_password: "test_password",
                                                 password: "test_update_pass",
                                                 password_confirmation: "test_wrong_pass")
-        patch password_path, params: { user: user_params }
+        patch password_path, params: { user: password_params }
         expect(response.status).to eq(200)
         expect(response.body).to include "再入力パスワードとパスワードの入力が一致しません"
       end
