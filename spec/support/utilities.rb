61d6aa33885d28e74cf5d1c94d7a4822a9a5e5ac
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-11-11 21:55:24 +0900

    [delete&fix] rubocopによる修正とlikesのmodel_spec削除

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -11,12 +11,12 @@ def post_message_and_image
   get root_path
   post_params = attributes_for(:no_images_post)
   post posts_path(username: user.user_name), params: {
-      post: {
-          message: post_params[:message],
-          images_attributes: {
-              "0": FactoryBot.attributes_for(:image)
-          }
+    post: {
+      message: post_params[:message],
+      images_attributes: {
+        "0": FactoryBot.attributes_for(:image)
       }
+    }
   }
 end
 
