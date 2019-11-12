7fb0fa19dd379d876aa076315410a24190488cab
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-11-12 21:51:51 +0900

    [fix] rubocopによる修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
   def create
     post = current_user.posts.build(post_params)
-# binding.pry
+    # binding.pry
     if post.save
       flash[:success] = "投稿に成功しました"
     else
