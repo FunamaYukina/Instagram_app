08e75ed703aa1025c8782aaeb04f8582042e3600
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-11-13 00:45:40 +0900

    counter_cacheの削除とruboによる修正

--- spec/requests/like_spec.rb
+++ spec/requests/like_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe "likes", type: :request do
       it "いいねができないこと" do
         expect do
           user
-          post like_path(username: user.user_name, post_id: 1), xhr: true, params: {post_id: 1, user_id: 1}
+          post like_path(username: user.user_name, post_id: 1), xhr: true, params: { post_id: 1, user_id: 1 }
         end.not_to change(Like, :count)
       end
     end
@@ -28,35 +28,36 @@ RSpec.describe "likes", type: :request do
 
       it "いいねしていない場合、いいねができること" do
         expect do
-          post like_path(username: user.user_name, post_id: 1), xhr: true, params: {post_id: 1, user_id: 1}
+          post like_path(username: user.user_name, post_id: 1), xhr: true, params: { post_id: 1, user_id: 1 }
         end.to change(Like, :count).by(1)
       end
 
       it "いいねボタンを２回押した場合、いいねが１回できること" do
         expect do
-          post like_path(username: user.user_name, post_id: 1), xhr: true, params: {post_id: 1, user_id: 1}
-          post like_path(username: user.user_name, post_id: 1), xhr: true, params: {post_id: 1, user_id: 1}
+          post like_path(username: user.user_name, post_id: 1), xhr: true, params: { post_id: 1, user_id: 1 }
+          post like_path(username: user.user_name, post_id: 1), xhr: true, params: { post_id: 1, user_id: 1 }
         end.to change(Like, :count).by(1)
       end
     end
 
     describe "#unlike" do
       let(:user) { create(:user, :with_post, :with_like) }
+
       before do
         log_in(user)
       end
 
       it "いいねしていた場合、いいねの解除ができること" do
         expect do
-          delete unlike_path(username: user.user_name, post_id: 1), xhr: true, params: {post_id: 1, user_id: 1}
+          delete unlike_path(username: user.user_name, post_id: 1), xhr: true, params: { post_id: 1, user_id: 1 }
         end.to change(Like, :count).by(-1)
       end
 
       it "いいね解除ボタンを２回押した場合、いいね解除が１回できること" do
         expect do
-          delete unlike_path(username: user.user_name, post_id: 1), xhr: true, params: {post_id: 1, user_id: 1}
+          delete unlike_path(username: user.user_name, post_id: 1), xhr: true, params: { post_id: 1, user_id: 1 }
           expect do
-            delete unlike_path(username: user.user_name, post_id: 1), xhr: true, params: {post_id: 1, user_id: 1}
+            delete unlike_path(username: user.user_name, post_id: 1), xhr: true, params: { post_id: 1, user_id: 1 }
           end.to raise_error(ActiveRecord::RecordNotFound)
         end.to change(Like, :count).by(-1)
       end
