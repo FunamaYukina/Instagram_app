2bbc11ef3aa2373f4ea7a87ca8cfac92382fabce
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-11-25 16:29:54 +0900

    [add] request_test追記,細かな修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,19 +2,13 @@
 
 class RelationshipsController < ApplicationController
   before_action :authenticated_user
-  before_action :set_variables
+  before_action :set_user
 
   def follow
-    current_user.follow(@user.id)
+    current_user.follow!(@user.id)
   end
 
   def unfollow
-    current_user.unfollow(@user.id)
+    current_user.unfollow!(@user.id)
   end
-
-  private
-
-    def set_variables
-      @user = User.find(params[:user_id])
-    end
 end
--- spec/requests/relationship_spec.rb
+++ spec/requests/relationship_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe "relationships", type: :request do
 
       it "フォローができないこと" do
         expect do
-          post follow_path(username: user.user_name), xhr: true, params: { username: user.user_name, user_id: user.id }
+          post follow_path(username: user.user_name), xhr: true, params: {username: user.user_name, user_id: user.id}
         end.not_to change(Relationship, :count)
       end
     end
@@ -28,8 +28,48 @@ RSpec.describe "relationships", type: :request do
 
       it "フォローできること" do
         expect do
-          post follow_path(username: another_user.user_name), xhr: true, params: { username: user.user_name, user_id: user.id }
-        end.to change(Relationship, :count)
+          post follow_path(username: another_user.user_name), xhr: true, params: {username: user.user_name, user_id: user.id}
+        end.to change(Relationship, :count).by(1)
+      end
+
+      it "フォローボタンを２回押した場合、フォローが１回できること" do
+        expect do
+          post follow_path(username: another_user.user_name), xhr: true, params: {username: user.user_name, user_id: user.id}
+          post follow_path(username: another_user.user_name), xhr: true, params: {username: user.user_name, user_id: user.id}
+        end.to raise_error(ActiveRecord::RecordInvalid)
+      end
+      it "自分自身をフォローできないこと" do
+        expect do
+          post follow_path(username: user.user_name), xhr: true, params: {username: user.user_name, user_id: user.id}
+        end.not_to change(Relationship, :count)
+      end
+    end
+  end
+
+  describe "#unfollow" do
+    context "未ログインの場合" do
+      it "フォロー解除するボタンが表示されないこと" do
+        get user_path(user.user_name)
+        expect(response.body).not_to include ".unfollow_button"
+      end
+      it "フォロー解除ができないこと" do
+        post follow_path(username: another_user.user_name), xhr: true, params: {username: user.user_name, user_id: user.id}
+        expect do
+          delete unfollow_path(username: another_user.user_name), xhr: true, params: {username: user.user_name, user_id: user.id}
+        end.not_to change(Relationship, :count)
+      end
+    end
+
+    context "ログイン済みの場合" do
+      before do
+        log_in(user)
+        post follow_path(username: another_user.user_name), xhr: true, params: {username: user.user_name, user_id: user.id}
+      end
+
+      it "フォロー解除できること" do
+        expect do
+          delete unfollow_path(username: another_user.user_name), xhr: true, params: {username: user.user_name, user_id: user.id}
+        end.to change(Relationship, :count).by(-1)
       end
     end
   end
