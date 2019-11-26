9a0232f99b95f792f7134bb64a0f87df95ca01a3
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-11-26 15:37:48 +0900

    [fix] いいね機能のテスト修正

--- spec/requests/like_spec.rb
+++ spec/requests/like_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe "likes", type: :request do
       it "いいねができないこと" do
         user
         expect do
-          post like_path(username: user.user_name, post_id: 1), xhr: true, params: { post_id: 1, user_id: 1 }
+          post like_path(username: user.user_name, post_id: 1), xhr: true
         end.not_to change(Like, :count)
         expect(response).to redirect_to login_path
       end
@@ -29,14 +29,14 @@ RSpec.describe "likes", type: :request do
 
       it "いいねしていない場合、いいねができること" do
         expect do
-          post like_path(username: user.user_name, post_id: 1), xhr: true, params: { post_id: 1, user_id: 1 }
+          post like_path(username: user.user_name, post_id: 1), xhr: true
         end.to change(Like, :count).by(1)
       end
 
       it "いいねボタンを２回押した場合、いいねが１回できること" do
         expect do
-          post like_path(username: user.user_name, post_id: 1), xhr: true, params: { post_id: 1, user_id: 1 }
-          post like_path(username: user.user_name, post_id: 1), xhr: true, params: { post_id: 1, user_id: 1 }
+          post like_path(username: user.user_name, post_id: 1), xhr: true
+          post like_path(username: user.user_name, post_id: 1), xhr: true
         end.to change(Like, :count).by(1)
       end
     end
@@ -50,15 +50,15 @@ RSpec.describe "likes", type: :request do
 
       it "いいねしていた場合、いいねの解除ができること" do
         expect do
-          delete unlike_path(username: user.user_name, post_id: 1), xhr: true, params: { post_id: 1, user_id: 1 }
+          delete unlike_path(username: user.user_name, post_id: 1), xhr: true
         end.to change(Like, :count).by(-1)
       end
 
       it "いいね解除ボタンを２回押した場合、いいね解除が１回できること" do
         expect do
-          delete unlike_path(username: user.user_name, post_id: 1), xhr: true, params: { post_id: 1, user_id: 1 }
+          delete unlike_path(username: user.user_name, post_id: 1), xhr: true
           expect do
-            delete unlike_path(username: user.user_name, post_id: 1), xhr: true, params: { post_id: 1, user_id: 1 }
+            delete unlike_path(username: user.user_name, post_id: 1), xhr: true
           end.to raise_error(ActiveRecord::RecordNotFound)
         end.to change(Like, :count).by(-1)
       end
