07a7875a977f2873211a59b1e414f3926ab1519c
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-11-29 23:57:22 +0900

    [add] pumaをバックグラウンドで実行する

--- config/puma.rb
+++ config/puma.rb
@@ -13,11 +13,22 @@ bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+# port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
+
+rails_root = Dir.pwd
+bind "unix://" + File.join(rails_root, "tmp", "sockets", "puma.sock")
+pidfile File.join(rails_root, "tmp", "pids", "puma.pid")
+state_path File.join(rails_root, "tmp", "pids", "puma.state")
+stdout_redirect(
+  File.join(rails_root, "log", "puma.log"),
+  File.join(rails_root, "log", "puma-error.log"),
+  true,
+)
+daemonize true
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
