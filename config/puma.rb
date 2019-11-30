ff6d7a51fd164081beb89ecb60235100c53255da
FunamaYukina <be-with-you1228@i.softbank.jp>
2019-11-30 13:38:18 +0900

    [add] localでもきちんとserverが起動するように

--- config/puma.rb
+++ config/puma.rb
@@ -17,17 +17,23 @@ bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+rails_env = environment ENV.fetch("RAILS_ENV") { "development" }
 
-rails_root = Dir.pwd
-pidfile File.join(rails_root, "tmp", "pids", "puma.pid")
-state_path File.join(rails_root, "tmp", "pids", "puma.state")
-stdout_redirect(
-  File.join(rails_root, "log", "puma.log"),
-  File.join(rails_root, "log", "puma-error.log"),
-  true,
-)
-daemonize true
+if rails_env == "development"
+  port ENV.fetch("PORT") { 3000 }
+end
+
+if rails_env == "production"
+  rails_root = Dir.pwd
+  pidfile File.join(rails_root, "tmp", "pids", "puma.pid")
+  state_path File.join(rails_root, "tmp", "pids", "puma.state")
+  stdout_redirect(
+      File.join(rails_root, "log", "puma.log"),
+      File.join(rails_root, "log", "puma-error.log"),
+      true,
+  )
+  daemonize true
+end
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
